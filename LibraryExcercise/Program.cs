452eb276cdd57bd0da494521b75a718ae0308014
Demetri Warren <dwarren@tql.com>
2024-06-19 10:19:53 -0400

    Add project files.

---
+++ LibraryExcercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using MyLibrary;
+
+namespace LibraryExcercise;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine(Class1.About);
+        
+        }
+    }
---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary
+{
+    public class Class1
+    {
+        public static string About { get; set; } = "My Class1"; // static means u dont need to create an instance to reference the var between the classes. 
+        
+
+
+    }
+}
